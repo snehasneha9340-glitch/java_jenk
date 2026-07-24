7c71c5acdf278980349adcb1096fe71a8afb9e13
snehasneha9340-glitch <snehasneha9340@gmail.com>
2026-07-24 12:02:12 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins");
         System.out.println("trigger build");
-        System.out.println("trigger build");
+        
     }
 }
